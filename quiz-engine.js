ca69e20fc0158e562ba95caf360e8490778181bd
Accueil ICP EIADE <sabah6@hotmail.fr>
2026-05-22 01:28:19 +0200

    Update quiz-engine.js

--- quiz-engine.js
+++ quiz-engine.js
@@ -260,26 +260,5 @@ currentQuestion++;
 showQuestion();
 
 });
-function openQuiz() {
 
-  // Ouvre le popup
-  document.getElementById("quizPopup").classList.add("show");
-
-  // Cache le bottom menu
-  document.getElementById("bottomMenu").style.display = "none";
-
-  // Lance la première question
-  currentQuestion = 0;
-  score = 0;
-
-  loadQuestion();
-}
-function closeQuiz() {
-
-  // Ferme popup
-  document.getElementById("quizPopup").classList.remove("show");
-
-  // Réaffiche le menu
-  document.getElementById("bottomMenu").style.display = "flex";
-}
 
