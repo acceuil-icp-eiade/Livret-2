b41e23f144e7b4b623a541c1c604e8a6ba7cd069
Accueil ICP EIADE <sabah6@hotmail.fr>
2026-05-22 10:01:59 +0200

    Update quiz-engine.js

--- quiz-engine.js
+++ quiz-engine.js
@@ -1,44 +1,71 @@
 /* =========================
-VARIABLES
+   DONNÉES QUIZ
 ========================= */
 
 let currentQuestion = 0;
 let score = 0;
 
+/* =========================
+   ELEMENTS HTML
+========================= */
+
 const stepEl = document.getElementById("step");
 const patientEl = document.getElementById("patient");
 const answersEl = document.getElementById("answers");
-
-const feedbackEl = document.getElementById("feedback");
-const correctEl = document.getElementById("correct");
-const nextBtn = document.getElementById("nextBtn");
-
 const progressEl = document.getElementById("progress");
 
 const scoreEl = document.getElementById("score");
 const restartBtn = document.getElementById("restartBtn");
+
 const feedbackPopup = document.getElementById("feedbackPopup");
 const feedbackTitle = document.getElementById("feedbackTitle");
 const feedbackText = document.getElementById("feedbackText");
 const continueBtn = document.getElementById("continueBtn");
 
 /* =========================
-AFFICHER QUESTION
+   OUVERTURE QUIZ
 ========================= */
+
+function openQuiz() {
+
+document.getElementById("quizPopup")
+.classList.add("show");
+
+}
+
+function closeQuiz() {
+
+document.getElementById("quizPopup")
+.classList.remove("show");
+
+}
+
+/* =========================
+   RESET UI
+========================= */
+
+function resetUI() {
+
+answersEl.innerHTML = "";
+
+}
+
+/* =========================
+   CHARGER QUESTION
+========================= */
+
 function loadQuestion() {
+
+resetUI();
 
 const q = quizData[currentQuestion];
 
 stepEl.textContent = q.step;
+
 patientEl.textContent = q.patient;
+
 progressEl.textContent =
 `Question ${currentQuestion + 1} / ${quizData.length}`;
-
-
-// 👉 reset UNE SEULE FOIS ici
-resetUI();
-
-nextBtn.style.display = "none";
 
 q.answers.forEach(answer => {
 
@@ -48,7 +75,9 @@ button.classList.add("answer-btn");
 
 button.textContent = answer.text;
 
-button.addEventListener("click", () => selectAnswer(button, answer));
+button.addEventListener("click", () => {
+selectAnswer(button, answer);
+});
 
 answersEl.appendChild(button);
 
@@ -57,198 +86,114 @@ answersEl.appendChild(button);
 }
 
 /* =========================
-REPONSE
-========*/
+   SELECTION REPONSE
+========================= */
 
 function selectAnswer(button, answer) {
 
-const buttons = document.querySelectorAll(".answer-btn");
-buttons.forEach(btn => btn.disabled = true);
+const buttons =
+document.querySelectorAll(".answer-btn");
+
+buttons.forEach(btn => {
+btn.disabled = true;
+});
 
 const q = quizData[currentQuestion];
-const correctAnswer = q.answers.find(a => a.correct);
+
+const correctAnswer =
+q.answers.find(a => a.correct);
 
 let isCorrect = false;
 
 if (answer.correct) {
+
 button.classList.add("correct");
+
 score++;
+
 scoreEl.textContent = score;
+
 isCorrect = true;
+
 } else {
+
 button.classList.add("wrong");
+
+buttons.forEach(btn => {
+
+if (btn.textContent === correctAnswer.text) {
+btn.classList.add("correct");
 }
-  
+
+});
+
+}
+
+/* =========================
+   MESSAGE FEEDBACK
+========================= */
+
 const explanation = answer.correct
-? "Bonne réponse 👍"
-: `${answer.feedback}<br><br>
+
+? `
+✅ Très bonne réponse.
+
+<p style="margin-top:15px;">
+${answer.feedback}
+</p>
+`
+
+: `
+<p>${answer.feedback}</p>
+
+<p style="margin-top:20px;">
 ✔ Bonne réponse :
-<strong>${correctAnswer.text}</strong>`;
+</p>
+
+<strong>
+${correctAnswer.text}
+</strong>
+`;
 
 showFeedback(isCorrect, explanation);
-}
 
+}
 
 /* =========================
-QUESTION SUIVANTE
+   POPUP FEEDBACK
 ========================= */
 
-nextBtn.addEventListener("click", () => {
-
-currentQuestion++;
-
-if(currentQuestion < quizData.length) {
-
-loadQuestion();
-
-} else {
-
-showFinalScreen();
-
-}
-
-});
-
-/* =========================
-FIN QUIZ
-========================= */
-function showFinalScreen() {
-
-// Centrer le titre
-stepEl.style.textAlign = "center";
-
-// Affichage fin
-stepEl.textContent = "🏁 Quiz terminé";
-patientEl.textContent = "";
-
-// Nettoyage réponses
-answersEl.innerHTML = "";
-
-// Nettoyage correction
-correctEl.innerHTML = "";
-
-// Progression vide
-progressEl.textContent = "";
-
-// Masquer bouton suivant
-nextBtn.style.display = "none";
-
-// Calcul pourcentage
-const percentage = Math.round((score / quizData.length) * 100);
-
-// Afficher score
-scoreEl.innerHTML = ⭐ Score : ${percentage}%;
-
-// Message dynamique
-if (percentage > 90) {
-
-feedbackEl.innerHTML = `  
-<h2 class="bravo-animation">🎉 Bravo !</h2>  
-  
-
-<p style="margin-top:15px; line-height:1.7;">  
-Excellent travail 👏<br>  
-Vous maîtrisez très bien les bases  
-de la communication hypnotique.  
-</p>  
-
-<p style="margin-top:20px; line-height:1.7;">  
-✅ Langage positif<br>  
-✅ Sécurité<br>  
-✅ Suggestions indirectes  
-</p>  
-`;
-
-} else {
-
-feedbackEl.innerHTML = `  
-<h2>💡 Continuez !</h2>  
-
-<p style="margin-top:15px; line-height:1.7;">  
-Vous êtes sur la bonne voie 👍<br>  
-Relancez le quiz pour améliorer  
-vos techniques de communication hypnotique.  
-</p>  
-
-<p style="margin-top:20px; line-height:1.7;">  
-✅ Langage positif<br>  
-✅ Sécurité<br>  
-✅ Suggestions indirectes  
-</p>  
-`;
-
-}
-
-// Afficher bouton rejouer
-restartBtn.style.display = "inline-block";
-}
-/*========≈============
-fermeture du popup
-======================*/
-continueBtn.addEventListener("click", () => {
-
-document.getElementById("feedbackPopup").classList.remove("show");
-
-currentQuestion++;
-
-if (currentQuestion < quizData.length) {
-loadQuestion();
-} else {
-showFinalScreen();
-}
-});
-
-/* =========================
-DEMARRAGE
-========================= */
-window.addEventListener("DOMContentLoaded", () => {
-loadQuestion();
-});
-restartBtn.addEventListener("click", () => {
-
-currentQuestion = 0;
-score = 0;
-
-// Reset score affiché
-scoreEl.textContent = "0";
-
-// Masquer bouton rejouer
-restartBtn.style.display = "none";
-
-// Recharger première question
-loadQuestion();
-
-});
-/==========≈============
-reset
-=====≈===============/
-
-function resetUI() {
-feedbackEl.textContent = "";
-correctEl.innerHTML = "";
-answersEl.innerHTML = "";
-}
-/*   nouveau popup réponse */
 function showFeedback(isCorrect, explanation) {
 
-feedbackPopup.style.display = "flex";
+feedbackPopup.classList.add("show");
 
 if (isCorrect) {
 
-feedbackTitle.innerHTML = "✅ Bonne réponse";
+feedbackTitle.innerHTML =
+"✅ Bonne réponse";
 
 } else {
 
-feedbackTitle.innerHTML = "❌ Réponse incorrecte";
+feedbackTitle.innerHTML =
+"❌ Réponse incorrecte";
 
 }
 
-feedbackText.innerHTML =   <p style="margin-top:15px; line-height:1.7;">   ${explanation}   </p>  ;
+feedbackText.innerHTML = `
+<div style="line-height:1.7;">
+${explanation}
+</div>
+`;
+
 }
+
+/* =========================
+   CONTINUER
+========================= */
 
 continueBtn.addEventListener("click", () => {
 
-feedbackPopup.style.display = "none";
+feedbackPopup.classList.remove("show");
 
 currentQuestion++;
 
@@ -261,5 +206,113 @@ loadQuestion();
 showFinalScreen();
 
 }
+
+});
+
+/* =========================
+   FIN QUIZ
+========================= */
+
+function showFinalScreen() {
+
+resetUI();
+
+stepEl.style.textAlign = "center";
+
+stepEl.innerHTML =
+"🏁 Quiz terminé";
+
+patientEl.innerHTML = "";
+
+progressEl.innerHTML = "";
+
+const percentage =
+Math.round((score / quizData.length) * 100);
+
+if (percentage >= 80) {
+
+answersEl.innerHTML = `
+
+<div class="final-message">
+
+<h2 class="bravo-animation">
+🎉 Bravo !
+</h2>
+
+<p style="margin-top:20px; line-height:1.8;">
+
+Excellent travail 👏
+
+<br><br>
+
+Vous maîtrisez très bien les bases
+de la communication hypnotique.
+
+</p>
+
+</div>
+
+`;
+
+} else {
+
+answersEl.innerHTML = `
+
+<div class="final-message">
+
+<h2>
+💡 Continuez !
+</h2>
+
+<p style="margin-top:20px; line-height:1.8;">
+
+Vous êtes sur la bonne voie 👍
+
+<br><br>
+
+Relancez le quiz pour améliorer
+vos techniques de communication.
+
+</p>
+
+</div>
+
+`;
+
+}
+
+scoreEl.innerHTML = `${percentage}%`;
+
+restartBtn.style.display = "inline-block";
+
+}
+
+/* =========================
+   RESTART
+========================= */
+
+restartBtn.addEventListener("click", () => {
+
+currentQuestion = 0;
+
+score = 0;
+
+scoreEl.textContent = "0";
+
+restartBtn.style.display = "none";
+
+stepEl.style.textAlign = "left";
+
+loadQuestion();
+
+});
+
+/* =========================
+   DEMARRAGE
+========================= */
+
+window.addEventListener("DOMContentLoaded", () => {
+
+loadQuestion();
 
 });
